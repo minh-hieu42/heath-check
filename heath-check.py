ac1de7a4d074f5b24055ed5f9877cd86b589efdd
minh-hieu42 <123865141+minh-hieu42@users.noreply.github.com>
2023-09-23 14:56:07 +0700

    add cpu_constrained check and rename the script

--- heath-check.py
+++ heath-check.py
@@ -23,11 +23,16 @@ def check_root_full():
     """Return True if the root partition is full, False otherwise."""
     return check_disk_full(disk="/", min_gb=2, min_percent=10)
 
+def check_cpu_constrained():
+    """Return True if the cpu is having too much usage, False otherwise."""
+    return psutil.cpu_percent(1) > 75
+
 
 def main():
     checks=[
         (check_reboot, "Pending Reboot"),
         (check_root_full, "Root partition full"),
+        (check_cpu_constrained, "CPU load too high."),
     ]
     everything_ok= True
     for check, msg in checks:
